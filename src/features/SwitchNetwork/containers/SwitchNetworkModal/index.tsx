a68208e8f5055c9e56f8c96cc237c844ae14aabb
Pavel Shapov <pavel.shapov@metalamp.io>
2022-10-22 22:51:38 +0600

    feat: add SwitchNetwork

---
+++ src/features/SwitchNetwork/containers/SwitchNetworkModal/index.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
+
+import { Modal } from 'shared/components/Modal';
+import { ENV } from 'shared/constants/env';
+import { Button } from 'shared/components/Button';
+
+import styles from './SwitchNetworkModal.module.scss';
+
+export const SwitchNetworkModal: FC = () => {
+  const { isConnected } = useAccount();
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+
+  return (
+    <>
+      {chain?.id !== ENV.PREFERRED_CHAIN_ID && isConnected && (
+        <Modal title="Switch the network">
+          <div className={styles['root']}>
+            <span className={styles['text']}>
+              It seems like your network is not Goerli. Please switch the network using your wallet or allow
+              our system to do it.
+            </span>
+
+            <div className={styles['button']}>
+              <Button onClick={switchNetwork?.bind(null, ENV.PREFERRED_CHAIN_ID)} uppercase stretch>
+                Switch to Goerli
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
+  );
+};
--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from 'react';
 import cn from 'classnames';
 
+import { SwitchNetworkModal } from 'features/SwitchNetwork/containers/SwitchNetworkModal';
+
 import { Footer } from './Footer';
 import { Header } from './Header';
 import styles from './BaseLayout.module.scss';
@@ -12,17 +14,21 @@ export type BaseLayoutProps = {
 
 export const BaseLayout: FC<BaseLayoutProps> = ({ children, gradient = 'linear' }) => {
   return (
-    <div className={styles['root']}>
-      <div className={styles['header']}>
-        <Header />
+    <>
+      <div className={styles['root']}>
+        <div className={styles['header']}>
+          <Header />
+        </div>
+        <main className={styles['main']}>{children}</main>
+        <div className={styles['footer']}>
+          <Footer />
+        </div>
+        <div className={cn(styles['gradient'], styles[`gradient--type_${gradient}`])}>
+          {gradient === 'diagonal' && <div className={styles['diagonal']} />}
+        </div>
       </div>
-      <main className={styles['main']}>{children}</main>
-      <div className={styles['footer']}>
-        <Footer />
-      </div>
-      <div className={cn(styles['gradient'], styles[`gradient--type_${gradient}`])}>
-        {gradient === 'diagonal' && <div className={styles['diagonal']} />}
-      </div>
-    </div>
+
+      <SwitchNetworkModal />
+    </>
   );
 };
