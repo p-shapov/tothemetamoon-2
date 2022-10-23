447dd95c7d4881696aab562dfd30cda12f44e2ec
Pavel Shapov <pavel.shapov@metalamp.io>
2022-10-24 05:28:27 +0600

    fix: debugging end-points

--- src/api/core.ts
+++ src/api/core.ts
@@ -5,10 +5,11 @@ import { axiosConfig } from './config';
 export const api = new Axios(axiosConfig);
 
 api.interceptors.response.use((val) => {
-  if (val.status !== 200) throw new Error(val.data);
-
-  // eslint-disable-next-line no-console
-  console.error(val.data);
+  if (val.status !== 200) {
+    // eslint-disable-next-line no-console
+    console.error(val.data);
+    throw new Error(val.data);
+  }
 
   return val;
 });
