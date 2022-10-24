d68e130d9dcb45a05873e1e9ec2cb7207301db19
Pavel Shapov <pavel.shapov@metalamp.io>
2022-10-24 06:55:49 +0600

    fix: debugging

--- src/shared/utils/getErrorMessage.ts
+++ src/shared/utils/getErrorMessage.ts
@@ -1,5 +1,4 @@
 export const getErrorMessage = (error: unknown) => {
   if (error instanceof Error) return error.message;
-  else if (typeof error === 'string') return error;
   else return 'Unknown error';
 };
